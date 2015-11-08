3da79d27d2fd7708954895a70b6073d2b80da314
tfbm89 <elrockerom5@hotmail.com>
2015-11-08 15:13:32 -0500

    reloj actualizado
    reloj actualizado

--- reloj/js/main.js
+++ reloj/js/main.js
@@ -1,60 +1,18 @@
-(function(){
-	var actualizarHora = function()
-	{
-		var fecha = new Date(),
-			horas = fecha.getHours(),
-			ampm,
-			minutos = fecha.getMinutes(),
-			segundos = fecha.getSeconds(),
-			diaSemana = fecha.getDay(),
-			dia = fecha.getDate(),
-			mes = fecha.getMonth(),
-			year = fecha.getFullYear();
+$(document).on('ready', inicio);
+function inicio()
+{	
 
-			var pHoras = document.getElementById('horas'),
-			    pAMPM =document.getElementById('ampm'),
-			    pMinutos = document.getElementById('minutos'),
-			    pSegundos = document.getElementById('segundos'),
-			    pDiaSemana = document.getElementById('diaSemana'),
-			    pDia = document.getElementById('dia'),
-			    pMes = document.getElementById('mes'),
-			    pYear = document.getElementById('year');
+	$('.titulo_galeria').on('click',mostrarGaleria);	
+	$('.cerrar').on('click',cerrarGaleria);	
+}
+	function cerrarGaleria(){
+	    $('.galeria_miniaturas').css('display','none');  
+	}
 
-			 var semana = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
-			 pDiaSemana.textContent = semana[diaSemana];
+	function mostrarGaleria(){
+	    $('.galeria_miniaturas').css('display','block');  
+	}
 
-			 pDia.textContent = dia;
+	
 
-			 var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-			 pMes.textContent = meses[mes];
-
-			 pYear.textContent = year;
-
-			 if (horas >= 12) 
-			 	{
-			 		horas = horas -12;
-			 		ampm = 'PM';
-			 	}else {
-			 		ampm = 'AM';
-			 	}
-
-			 	if (horas == 0)
-			 	 {
-			 	 	horas = 12;
-			 	 };
-
-			 	 pHoras.textContent = horas;
-			 	 pAMPM.textContent = ampm;
-
-			 	 if (minutos < 10){minutos = "0" + minutos};
-			 	 if (segundos < 10){segundos = "0" + segundos};
-
-			 	 pMinutos.textContent = minutos;
-			 	 pSegundos.textContent = segundos;
-
-	};
-
-	actualizarHora();
-	var intervalo =setInterval(actualizarHora, 1000);
-
-}())	
+	

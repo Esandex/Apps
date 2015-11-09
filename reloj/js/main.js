f0f865e97e6a6a5d015cdfd150bdfbefcc63ba90
burngeek8 <burngeek8@gmail.com>
2015-11-09 00:32:42 -0500

    unos cuantos cambios :)

--- reloj/js/main.js
+++ reloj/js/main.js
@@ -1,18 +1,32 @@
 $(document).on('ready', inicio);
 function inicio()
 {	
-
-	$('.titulo_galeria').on('click',mostrarGaleria);	
-	$('.cerrar').on('click',cerrarGaleria);	
+	$('.boton').on('click', menu);	
 }
-	function cerrarGaleria(){
-	    $('.galeria_miniaturas').css('display','none');  
-	}
-
-	function mostrarGaleria(){
-	    $('.galeria_miniaturas').css('display','block');  
-	}
-
 	
+function menu(){
+	var appMenu = $('.appMenu')
+	var distancia =  appMenu.css('right');
 
+	if(distancia == '0px'){
+		console.log('abierto');
+		$('.appMenu').css('right', '-150px');	
+		$('.appMenu .header .boton').addClass('menu');
+		$('.appMenu .header .boton').removeClass('cerrar');
+	}else{
+		console.log('cerrado');
+		$('.appMenu').css('right', '0px');	
+		$('.appMenu .header .boton').addClass('cerrar');
+		$('.appMenu .header .boton').removeClass('menu');
+	}
+}
+
+function cambiarImg(obj){
+	var objeto = obj;
+	var css = {
+		'background-image': 'url(img/' + objeto + ')'
+	}
+	console.log(css);
+	$('body').css(css);
+}
 	
